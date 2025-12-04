91b7a1e484e73a7cf88c9222a870d4a60918c7df
greatkevingkpro-svg <greatkevingkpro@gmail.com>
2025-12-04 16:14:27 +0300

    conditions and if statement

---
+++ conditions.js
@@ -0,0 +1,19 @@
+let firstName = "kevin";
+
+// if(firstName == "hana" || firstName == "kevin") {
+//   console.log("You have won a car");
+// } else {
+//   console.log("you have won a bicycle");
+// }
+
+switch (firstName) {
+  case "Axcel":
+    console.log("hey you have a nice car");
+    break;
+  case "Isaac":
+    console.log("you car is not nice");
+    break;
+  default:
+    console.log("try again");
+
+}
