c58f1ad1391dbb7e779a8728ec099a557cf22fbe
greatkevingkpro-svg <greatkevingkpro@gmail.com>
2026-01-16 13:43:27 +0300

    shipping calculator

---
+++ shipping-calculator.js
@@ -0,0 +1,17 @@
+function handleCostKeyDown(event) {
+  if (event.key === "Enter") {
+    calculateTotal();
+  }
+}
+
+function calculateTotal() {
+  const inputElement = document.querySelector(".js-cost-input");
+  let cost = Number(inputElement.value);
+
+  if (cost < 40) {
+    cost = cost + 10;
+    console.log(cost);
+  }
+
+  document.querySelector(".js-total-cost").innerHTML = `$${cost}`;
+}
